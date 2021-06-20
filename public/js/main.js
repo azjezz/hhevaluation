c621902a35837a7d6d77df4724941af7fcac9740
azjezz <azjezz@protonmail.com>
2021-06-20 09:52:40 +0100

    load results for all versions in the background
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,41 @@
 (function () {
   document.addEventListener('DOMContentLoaded', async () => {
     if (document.location.pathname.startsWith('/c/')) {
+      /** @type {String} selector */
+      let identifier = document.getElementById('main-container').dataset.identifier;
+      /** @type {HTMLSelectElement} selector */
+      let selector = document.getElementById('hhvm-version-selector');
+
+      let version;
+      let promises = {};
+      for (option of selector.options) {
+        /**
+         * @type {HTMLOptionElement} option
+         */
+        version = option.value;
+
+        promises[version] = (async (version) => {
+          let runtime = await fetch('/r/' + identifier + '/' + version).then((response) => response.json());
+          let type_checker = await fetch('/t/' + identifier + '/' + version).then((response) => response.json());
+
+          return { runtime: runtime, type_checker: type_checker };
+        })(version);
+      }
 
       let update = async () => {
-        /** @type {HTMLSelectElement} selector */
-        let selector = document.getElementById('hhvm-version-selector');
-        /** @type {String} selector */
-        let identifier = document.getElementById('main-container').dataset.identifier;
-
         selector.disabled = true;
-        await update_results(identifier, selector.value);
+
+        await update_results(selector.value, promises);
 
         let url = new URL(window.location);
         url.searchParams.set('version', selector.value);
-
         window.history.pushState({}, '', url);
 
         selector.disabled = false;
       };
 
-      document.getElementById('hhvm-version-selector').addEventListener('change', update);
+      selector.addEventListener('change', update);
+
 
       await update();
     }
@@ -27,48 +43,44 @@
 })();
 
 /**
- * @param {String} identifier
  * @param {String} version
  * 
  * @return {Promise<void>}
  */
-async function update_results(identifier, version) {
+async function update_results(version, promises) {
   await Promise.all([
-    update_runtime_result(identifier, version),
-    update_type_checker_result(identifier, version)
+    update_runtime_result(version, promises),
+    update_type_checker_result(version, promises)
   ]);
 }
 
 /**
- * @param {String} identifier
  * @param {String} version
  * 
  * @return {Promise<void>}
  */
-async function update_runtime_result(identifier, version) {
+async function update_runtime_result(version, promises) {
   document.getElementById('runtime.animation').classList.remove('hidden');
 
   document.getElementById('runtime.stdout').parentElement.classList.add('hidden');
   document.getElementById('runtime.stderr').parentElement.classList.add('hidden');
   document.getElementById('runtime.version_details').parentElement.classList.add('hidden');
 
-  let response = await fetch('/r/' + identifier + '/' + version);
+  let result = await promises[version];
 
-  let result = await response.json();
-
-  document.getElementById('runtime.stdout').innerText = result.stdout_content.trimStart();
-  document.getElementById('runtime.stderr').innerText = result.stderr_content.trimStart();
-  document.getElementById('runtime.version_details').innerText = result.detailed_version.trimStart();
+  document.getElementById('runtime.stdout').innerText = result.runtime.stdout_content.trimStart();
+  document.getElementById('runtime.stderr').innerText = result.runtime.stderr_content.trimStart();
+  document.getElementById('runtime.version_details').innerText = result.runtime.detailed_version.trimStart();
 
   document.getElementById('runtime.animation').classList.add('hidden');
 
   // keep the element hidden if the content is empty.
-  if ('' !== result.stdout_content.trim()) {
+  if ('' !== result.runtime.stdout_content.trim()) {
     document.getElementById('runtime.stdout').parentElement.classList.remove('hidden');
   }
 
   // keep the element hidden if the content is empty.
-  if ('' !== result.stderr_content.trim()) {
+  if ('' !== result.runtime.stderr_content.trim()) {
     document.getElementById('runtime.stderr').parentElement.classList.remove('hidden');
   }
 
@@ -76,35 +88,31 @@ async function update_runtime_result(identifier, version) {
 }
 
 /**
- * @param {String} identifier
  * @param {String} version
  * 
  * @return {Promise<void>}
  */
-async function update_type_checker_result(identifier, version) {
+async function update_type_checker_result(version, promises) {
   document.getElementById('type_checker.animation').classList.remove('hidden');
 
   document.getElementById('type_checker.stdout').parentElement.classList.add('hidden');
   document.getElementById('type_checker.stderr').parentElement.classList.add('hidden');
   document.getElementById('type_checker.version_details').parentElement.classList.add('hidden');
 
-  let response = await fetch('/t/' + identifier + '/' + version);
+  let result = await promises[version];
 
-  let result = await response.json();
-
-  document.getElementById('type_checker.stdout').innerText = result.stdout_content.trimStart();
-  document.getElementById('type_checker.stderr').innerText = result.stderr_content.trimStart();
-  document.getElementById('type_checker.version_details').innerText = result.detailed_version.trimStart();
+  document.getElementById('type_checker.stdout').innerText = result.type_checker.stdout_content.trimStart();
+  document.getElementById('type_checker.stderr').innerText = result.type_checker.stderr_content.trimStart();
+  document.getElementById('type_checker.version_details').innerText = result.type_checker.detailed_version.trimStart();
 
   document.getElementById('type_checker.animation').classList.add('hidden');
 
-
   // keep the element hidden if the content is empty.
-  if ('' !== result.stdout_content.trim()) {
+  if ('' !== result.type_checker.stdout_content.trim()) {
     document.getElementById('type_checker.stdout').parentElement.classList.remove('hidden');
   }
 
-  if ('' !== result.stderr_content.trim()) {
+  if ('' !== result.type_checker.stderr_content.trim()) {
     document.getElementById('type_checker.stderr').parentElement.classList.remove('hidden');
   }
 

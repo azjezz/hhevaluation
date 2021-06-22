420cf675b5faa6753b6d1cef21bef7fb66946be3
azjezz <azjezz@protonmail.com>
2021-06-22 01:48:20 +0100

    add warmup script
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- bin/build.sh
+++ bin/build.sh
@@ -10,3 +10,10 @@ hhvm --hphp -t hhbc --input-dir . -o build
 
 echo "hhvm.repo.authoritative = true" >> server.ini
 echo "hhvm.repo.central.path = \""$(pwd)"/build/hhvm.hhbc\"" >> server.ini
+
+rm var/logs.txt
+
+hhvm -m daemon -c server.ini
+
+# warmup the JIT :)
+ab -c 100 -n 100000 http://localhost:8080/

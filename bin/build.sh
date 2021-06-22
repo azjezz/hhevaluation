d58471dbdd6fc9ea680df14d542c3878f880cd6c
azjezz <azjezz@protonmail.com>
2021-06-22 01:15:22 +0100

    update configurations
    Signed-off-by: azjezz <azjezz@protonmail.com>

--- bin/build.sh
+++ bin/build.sh
@@ -8,6 +8,5 @@ rm -f vendor/autoload.php
 rm .gitattributes .gitignore composer.json composer.lock hh_autoload.json hhast-lint.json README.md
 hhvm --hphp -t hhbc --input-dir . -o build
 
-echo "
-hhvm.repo.authoritative = true
-hhvm.repo.central.path = \""$(pwd)"/build/hhvm.hhbc\"" >> server.ini
+echo "hhvm.repo.authoritative = true" >> server.ini
+echo "hhvm.repo.central.path = \""$(pwd)"/build/hhvm.hhbc\"" >> server.ini
